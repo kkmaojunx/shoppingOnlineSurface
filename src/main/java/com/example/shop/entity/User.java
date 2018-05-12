aa10d54559623acbe56167bf003e97bf410d841b
yanghuankun <yhk19970220@163.com>
2018-05-12 17:45:03 +0800

    <commit>:'asfd'

--- src/main/java/com/example/shop/entity/User.java
+++ src/main/java/com/example/shop/entity/User.java
@@ -82,9 +82,7 @@ public class User implements Serializable {
     public Date getBirthday() {
         //这里由于有夏令营时间存在 这是要默认设置时区，@see http://www.cnblogs.com/memory4young/p/java-timezone.html
         TimeZone.setDefault(TimeZone.getTimeZone("GMT+08"));
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        simpleDateFormat.format(birthday);
-        return new Date(simpleDateFormat.toString());
+        return birthday;
     }
 
     public void setBirthday(Date birthday) {
