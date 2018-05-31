96465f4c3b53b62d841759f7009b12c58e380d4f
yanghuankun <yhk19970220@163.com>
2018-05-31 14:51:11 +0800

    <commit>:'asfd'

--- src/main/java/com/example/shop/controller/ActivityController.java
+++ src/main/java/com/example/shop/controller/ActivityController.java
@@ -1,15 +1,15 @@
 package com.example.shop.controller;
 
 import com.example.shop.entity.Activity;
+import com.example.shop.entity.Shopping;
 import com.example.shop.server.ActivityService;
+import com.example.shop.server.ShoppingService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 活动类controller
@@ -20,15 +20,33 @@ public class ActivityController {
 
     @Resource
     private ActivityService activityService;
+    @Resource
+    private ShoppingService shoppingService;
 
     /**
      * 查询所有活动
+     *
      * @return
      */
     @RequestMapping("/list")
     public Map<String, Object> activityList(@RequestParam(value = "id", required = false) Integer id) {
         Map<String, Object> stringObjectMap = new HashMap<>();
-        List<Activity> activities = activityService.activityList(id);
+        Shopping shopping = new Shopping();
+        shopping.setActivity(id);
+        List<Shopping> shoppingList = shoppingService.activityShoppingList(shopping);
+        List<Activity> activities = new ArrayList<>();
+        if (shoppingList != null && shoppingList.size() > 0) {
+            Iterator iterator = shoppingList.iterator();
+            while (iterator.hasNext()) {
+                Shopping shopping1 = (Shopping) iterator.next();
+                if (shopping1 != null) {
+                    Activity activity = new Activity();
+                    activity.setShopid(shopping1.getId());
+                    activity.setImageUrl(shopping1.getIpAddress() + shopping1.getActivity_img());
+                    activities.add(activity);
+                }
+            }
+        }
         stringObjectMap.put("code", 1);
         stringObjectMap.put("msg", "成功");
         stringObjectMap.put("info", activities);
